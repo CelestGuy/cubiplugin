882d428fd26943375656420bc33b252066d38b31
Théo Duval <theo.duval27@outlook.fr>
2023-04-06 17:45:10 +0200

    Racing

--- src/main/java/net/ddns/lagarderie/cubiplugin/Main.java
+++ src/main/java/net/ddns/lagarderie/cubiplugin/Main.java
@@ -1,14 +1,10 @@
 package net.ddns.lagarderie.cubiplugin;
 
-import net.ddns.lagarderie.cubiplugin.commands.CommandInfos;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Objects;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        Objects.requireNonNull(getCommand("infos")).setExecutor(new CommandInfos());
     }
     @Override
     public void onDisable() {
---
+++ src/main/java/net/ddns/lagarderie/cubiplugin/game/Racing.java
@@ -0,0 +1,4 @@
+package net.ddns.lagarderie.cubiplugin.game;
+
+public class Racing {
+}
